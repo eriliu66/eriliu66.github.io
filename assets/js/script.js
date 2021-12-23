19a114aa7793fdd221e391cc45d3906e3e20f549
eriliu66 <95333668+eriliu66@users.noreply.github.com>
2021-12-23 13:54:45 -0800

    v2

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,6 +1,53 @@
-/* about page hover effect */
+/* about page */
 
-let set1 = document.getElementById("set1");
+let picset1 = document.getElementById("pic-set-1");
+let picset2 = document.getElementById("pic-set-2");
+let picset3 = document.getElementById("pic-set-3");
+
+let img1 = document.getElementById("img1");
+let img2 = document.getElementById("img2");
+let img3 = document.getElementById("img3");
+let pd1 = document.getElementById("pd1");
+let pd2 = document.getElementById("pd2");
+let pd3 = document.getElementById("pd3");
+
+function description1() {
+    if (window.getComputedStyle(img1).getPropertyValue("opacity") === "1") {
+        img1.style.opacity = "0";
+        pd1.style.opacity = "1";
+    } else {
+        img1.style.opacity = "1";
+        pd1.style.opacity = "0";
+    }
+}
+
+picset1.onclick = description1;
+
+function description2() {
+    if (window.getComputedStyle(img2).getPropertyValue("opacity") === "1") {
+        img2.style.opacity = "0";
+        pd2.style.opacity = "1";
+    } else {
+        img2.style.opacity = "1";
+        pd2.style.opacity = "0";
+    }
+}
+
+picset2.onclick = description2;
+
+function description3() {
+    if (window.getComputedStyle(img3).getPropertyValue("opacity") === "1") {
+        img3.style.opacity = "0";
+        pd3.style.opacity = "1";
+    } else {
+        img3.style.opacity = "1";
+        pd3.style.opacity = "0";
+    }
+}
+
+picset3.onclick = description3;
+
+/*let set1 = document.getElementById("set1");
 let set2 = document.getElementById("set2");
 let set3 = document.getElementById("set3");
 
@@ -43,4 +90,4 @@ set1.onmouseout = hideBlackOverlay1;
 set2.onmouseover = unhideBlackOverlay2;
 set2.onmouseout = hideBlackOverlay2;
 set3.onmouseover = unhideBlackOverlay3;
-set3.onmouseout = hideBlackOverlay3;
+set3.onmouseout = hideBlackOverlay3;*/
